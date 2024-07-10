8cda45bead09bafb42bc01f9bb90134ba3dab691
roman123org <oksanaromanko5@gmail.com>
2024-07-10 13:57:17 +0300

    add numbers

--- Solution1.java
+++ Solution1.java
@@ -5,4 +5,13 @@ public class Solution1 {
             }
             return (n & (n - 1)) == 0;
         }
+        public static void main(String[] args) {
+            Solution1 solution1 = new Solution1();
+            int n1 = 2;
+            System.out.println(solution1.isPowerOfTwo(n1));
+            int n2 = 4;
+            System.out.println(solution1.isPowerOfTwo(n2));
+            int n3 = 5;
+            System.out.println(solution1.isPowerOfTwo(n3));
+        }
     }
